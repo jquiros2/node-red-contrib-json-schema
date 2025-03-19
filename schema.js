8456c3467549a8018b9900b233f7d096fcb191d2
jquiros1 <jon@quirosabi.com>
2025-03-19 09:37:03 -0600

    test1

--- schema.js
+++ schema.js
@@ -1,42 +1,30 @@
 module.exports = function(RED) {
-    "use strict";
-    function JsonSchemaValidator(n) {
-        RED.nodes.createNode(this, n);
-        this.func = n.func;
-        this.name = n.name;
-		this.property = n.property;
-		this.propertyType = n.propertyType;
-		
+    function JsonFullSchemaValidatorNode(config) {
+        RED.nodes.createNode(this, config);
         var node = this;
+        node.name = config.name;
+        node.property = config.property;
+        node.propertyType = config.propertyType;
+        node.func = config.func;
+        node.precision = config.precision || 1; // Default precision value
 
-        var Ajv = require('ajv');
-        var ajv = Ajv({
-            allErrors: true,
-            messages: true
-        });
-        ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
-        
         node.on('input', function(msg) {
-			var prop = RED.util.evaluateNodeProperty(node.property,node.propertyType,node,msg);
-			
-            if (prop !== undefined) {
-                var schema = typeof node.func === 'string' && node.func.trim().length ? JSON.parse(node.func) : typeof msg.schema === 'string' ? JSON.parse(msg.schema) : msg.schema;
-                var validate = ajv.compile(schema);
-                var valid = validate(prop);
-                if (!valid) {
-                    msg['error'] = validate.errors;
-                    //node.error('Invalid JSON', msg);
-                    node.send([null,msg]);
-                }
-                else {
-                    node.send([msg,null]);
-                }
-            } else {
-                node.send([null,msg]);
-				//node.error('property of type \'' + node.propertyType + '\' and name \'' + node.property + '\' is undefined', msg);
-			}
-        });
+            var property = RED.util.evaluateNodeProperty(node.property, node.propertyType, node, msg);
+            var precision = node.precision;
 
+            // Use the precision value in your logic
+            // For example, if you are calling ajv, include the precision option
+            var ajv = new Ajv({ multipleOfPrecision: precision, strict: false });
+            var validate = ajv.compile(schema);
+            var valid = validate(property);
+
+            if (!valid) {
+                msg.error = validate.errors;
+                node.send([null, msg]);
+            } else {
+                node.send([msg, null]);
+            }
+        });
     }
-    RED.nodes.registerType("json-full-schema-validator", JsonSchemaValidator);
-};
+    RED.nodes.registerType("json-full-schema-validator", JsonFullSchemaValidatorNode);
+}
